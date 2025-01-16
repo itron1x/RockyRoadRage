a81a7a50941f062b7d4d37bcc22ed94e7f93bfc6
tmauritz <146174315+tmauritz@users.noreply.github.com>
2025-01-16 22:32:40 +0100

    Leaderboard now switches between local(race) and global(map) leaderboard

--- Assets/Scripts/LeaderboardScript.cs
+++ Assets/Scripts/LeaderboardScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using TMPro;
 using UnityEditor;
+using UnityEditor.U2D.Sprites;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
@@ -13,7 +14,10 @@ public class LeaderboardScript : MonoBehaviour
     [SerializeField] private SceneAsset mainMenuScene;
     
     [Header("Leaderboard")]
+    [SerializeField] private TextMeshProUGUI leaderboardHeader;
     [SerializeField] private List<TextMeshProUGUI> leaderboardTexts;
+    
+    private List<LeaderBoardEntry> _raceleaderboard = new List<LeaderBoardEntry>();
     
     public void OnBackToMainMenu()
     {
@@ -21,14 +25,39 @@ public class LeaderboardScript : MonoBehaviour
         SceneManager.LoadScene(mainMenuScene.name);
     }
     
-    public void SetLeaderboard(List<LeaderBoardEntry> leaderboardEntries)
+    public void SetRaceLeaderboard(List<LeaderBoardEntry> leaderboardEntries)
     {
+        _raceleaderboard = leaderboardEntries;
+    }
+
+    public void ShowRaceLeaderboard()
+    {
+        ClearLeaderboard();
+        leaderboardHeader.text = "Race Leaderboard";
+        for (int i = 0; i < Math.Min(leaderboardTexts.Count, _raceleaderboard.Count); i++)
+        {
+            DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(_raceleaderboard[i].time).DateTime;
+            leaderboardTexts[i].text = (i + 1) + ".\t " + _raceleaderboard[i].name + "\t\t" + dateTime.ToString("mm:ss.fff");
+        }
+        Invoke("ShowGlobalLeaderboard", 10f);
+    }
+
+    public void ShowGlobalLeaderboard()
+    {
+        ClearLeaderboard();
+        RaceInfoSystem infoSystem = RaceInfoSystem.GetInstance();
+        List<LeaderBoardEntry> leaderboardEntries = infoSystem.GetGlobalLeaderboardData(infoSystem.ActiveMapIndex);
+        leaderboardHeader.text = "Map Leaderboard";
         for (int i = 0; i < Math.Min(leaderboardTexts.Count, leaderboardEntries.Count); i++)
         {
             DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(leaderboardEntries[i].time).DateTime;
-            leaderboardTexts[i].text = (i+1)+".\t "+leaderboardEntries[i].name+"\t\t"+dateTime.ToString("mm:ss.fff");
+            leaderboardTexts[i].text = (i + 1) + ".\t " + leaderboardEntries[i].name + "\t\t" + dateTime.ToString("mm:ss.fff");
         }
-        SaveSystem.SaveLeaderboard();
+        Invoke("ShowRaceLeaderboard", 10f);
     }
-    
+
+    private void ClearLeaderboard()
+    {
+        for (int i = 0; i < leaderboardTexts.Count; i++)leaderboardTexts[i].text = "";
+    }
 }
